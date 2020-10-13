cc028da92835525c3d5f46b2f0e02abf66f663c8
mjmccoach <mmccoach88@gmail.com>
2020-10-13 20:29:28 +0100

    MVP finished

--- models/decorator.js
+++ models/decorator.js
@@ -6,4 +6,25 @@ Decorator.prototype.addStock= function(addedStock){
     this.stock += addedStock;
 }
 
+Decorator.prototype.calculateLitres = function(paintCan) {
+    let totalLitres = this.stock * paintCan.litres;
+    return totalLitres 
+}
+
+Decorator.prototype.enoughPaint = function(room) {
+    if (this.stock >= room.area) {
+        return true
+    } else {
+        return false
+    }
+}
+
+Decorator.prototype.enoughStock = function(room) {
+    if((this.stock * 2) >= room.area) {
+        return true
+    } else {
+        return false
+    }
+}
+
 module.exports = Decorator;
--- models/specs/decorator_test.js
+++ models/specs/decorator_test.js
@@ -1,11 +1,17 @@
 const assert = require('assert');
 const Decorator = require('../decorator.js');
+const PaintCan = require('../paint_can.js')
+const Room = require('../room.js')
 
 describe('Decorator', function(){
     let decorator;
+    let paintCan;
+    let room;
 
     beforeEach(function(){
         decorator = new Decorator();
+        paintCan = new PaintCan(2)
+        room = new Room(5)
     });
     
     it('decorator should start with 0 stock', function(){
@@ -18,4 +24,23 @@ describe('Decorator', function(){
         const actual = decorator.stock;
         assert.strictEqual(actual, 5);
     });
+    
+    it('calculate stock in litres', function(){
+        decorator.addStock(5);
+        const actual = decorator.calculateLitres(paintCan);
+        assert.strictEqual(actual, 10)
+    });
+
+    it ('is there enough paint?', function(){
+        decorator.addStock(5);
+        decorator.calculateLitres(paintCan) 
+        const actual = decorator.enoughPaint(room)
+        assert.strictEqual(actual, true)
+    });
+    it('is there enough stock?', function(){
+        decorator.addStock(5);
+        const actual = decorator.enoughStock(room)
+        assert.strictEqual(actual, true)
+    })
+
 })
